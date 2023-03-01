6b6f0a865642253b44cc13a06fa860be6909c29f
brandon <brandon14ogola@gmail.com>
2023-03-01 10:53:28 +0300

    commit tree 887759af2055afc4213a22c73979db1012ba8936

--- spec/methods_spec.rb
+++ spec/methods_spec.rb
@@ -54,27 +54,3 @@ describe '#halve' do
 
 end
 
-def greet_programmer()
-  puts "Hello, programmer!"
-end
-
-def greet(name = "Naureen")
-  puts "Hello, #{name}!"
-end 
-
-def greet_with_default(name = "programmer")
-  puts "Hello, #{name}!"
-end  
-
-def add(num1, num2)
-  return num1 + num2
-end  
-
-def halve(x)
- if x.is_a?(Integer)
-    x / 2.0
-  else
-    nil
-  end
-end
-
